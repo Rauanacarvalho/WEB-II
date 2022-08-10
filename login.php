03049c3ee1bc3652166e7802f71be77ed56b99ea
Rauanacarvalho <92538266+Rauanacarvalho@users.noreply.github.com>
2022-08-10 07:37:27 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,13 @@
 <?php
 
-  if  (isset($_POST['login'])) {
-      setcookie('login', $_POST['login']);
-  }
-
-
   if (!isset($_COOKIE['login'])) {
     include 'login.html';
+  }
 
     function criarSessaoCliente(){
     session_start();
     $_SESSION["login"]=$_POST["login"];
-    header(‘Location:areacliente.php’);
+    header(‘Location:areadocliente.php’);
 exit;
   }
 
